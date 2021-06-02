e410040ed2a256b3f4cbe3125c3d6643a0a6b189
afifsubarkah <yonasmullas@gmail.com>
2021-06-02 17:56:10 +0700

    perubahan website

--- contact-me.js
+++ contact-me.js
@@ -1,3 +1,30 @@
-$(function() {
-            console.log("ghi");
+$(function(){
+
+    $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event,errors){
+
+        },
+        submitSuccess:function($form, event){
+            event.preventDefault();
+
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var subject = $("input#subject").val();
+            var message = $("textarea#message").val();
         }
+        $this = $("#senMessageButton");
+        $this.prop("disabled",true);
+        $.ajax({
+            url:"././mail/contact-me.php",
+            type:"POST",
+            data{
+                name:name,
+                email:email,
+                subject:subject,
+                message:message,
+            },
+            
+        })
+    })
+}
